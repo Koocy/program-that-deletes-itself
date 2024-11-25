e3a4ab31009b6b608effa48e0b29cbb533fa9953
Koocy <anakullanici1@gmail.com>
2024-11-25 21:35:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (StreamWriter sw = new StreamWriter("hello.bat", false))
+            {
+                sw.WriteLine("timeout 1");
+                sw.WriteLine("del /f " + System.AppDomain.CurrentDomain.FriendlyName);
+                sw.WriteLine("start cmd.exe /c cd " + System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + " && del /f hello.bat");
+            };
+
+            Process This = Process.GetCurrentProcess();
+
+            Process.Start("cmd.exe", "/c hello.bat");
+
+            This.Kill();
+
+            Console.WriteLine("got here");
+            Console.Read();
+        }
+    }
+}
